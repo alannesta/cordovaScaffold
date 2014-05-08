4429e380620c77157061df84138ff7583d303a27
Alan <acao@moneycloud.me>
2014-05-07 22:21:35 -0400

    initialize cordova and angular scaffold project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('cordovaTestApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
